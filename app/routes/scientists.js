b9b37e52dc16bcbe52e6b2b2f49e7fa1474e77e6
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-17 12:58:40 -0400

    model function added to the route file.
    If you need to fetch data asynchronously, the model() method supports any library that uses JavaScript Promises.

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,4 +1,8 @@
+//app/routes/scientists.js
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  }
 });
