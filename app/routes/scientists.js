62de3328db4eff79df9d09bfd7e67541aa50a530
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-17 13:00:37 -0400

    model() method supports libraries that use promise! also browserfi needed for axios import.

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,8 +1,15 @@
 //app/routes/scientists.js
 import Route from '@ember/routing/route';
+import axios from 'npm:axios';
 
 export default Route.extend({
   model() {
-    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+    return axios.get('https://api.github.com/users/github/repos?page=1')
+    .then(function (response) {
+      return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann', JSON.stringify(response)];
+    })
+    .catch(function (error) {
+      return ([JSON.stringify(error)]);
+    });
   }
 });
